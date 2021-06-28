9639303f9982b5e53778f3ca1074cd34a88c484e
Mauro Damian Perez Garcia <47151811+mauropgarcia1996@users.noreply.github.com>
2021-06-28 15:50:52 -0300

    updated contract

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -4,7 +4,6 @@ pragma solidity >0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Swapper {
-    address public fromAddress;
     IERC20 public fromToken;
     IERC20 public toToken;
     uint256 public amount;
